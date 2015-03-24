500ba865b4c87ad46b8d8674b111a29044a8bf70
songm <songm@rose-hulman.edu>
2015-03-24 14:19:18 -0400

    Created PrimeNumberGenerator class and test class

---
+++ src/PrimeNumberGenerator.java
@@ -0,0 +1,10 @@
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author songm.
+ *         Created Mar 24, 2015.
+ */
+public class PrimeNumberGenerator {
+
+}
---
+++ src/PrimeNumberGeneratorTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author songm.
+ *         Created Mar 24, 2015.
+ */
+public class PrimeNumberGeneratorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
