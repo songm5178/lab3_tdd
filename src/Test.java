351171a404519f3d671584fc9d4855dbc921af6b
songm <songm@rose-hulman.edu>
2015-03-24 13:51:27 -0400

    Added JUnit File

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author songm.
+ *         Created Mar 24, 2015.
+ */
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
