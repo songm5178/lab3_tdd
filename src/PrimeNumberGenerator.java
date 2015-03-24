3c9845eec6c18dc2e20c46afb95525cd4bc082c5
songm <songm@rose-hulman.edu>
2015-03-24 13:59:27 -0400

    Added test 1

--- src/PrimeNumberGenerator.java
+++ src/PrimeNumberGenerator.java
@@ -1,10 +1,13 @@
+import java.util.List;
 
 /**
  * TODO Put here a description of what this class does.
  *
- * @author songm.
- *         Created Mar 24, 2015.
+ * @author songm. Created Mar 24, 2015.
  */
 public class PrimeNumberGenerator {
 
+	public static List<Integer> generate(int i) {
+		return null;
+	}
 }
--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,29 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * TODO Put here a description of what this class does.
  *
- * @author songm.
- *         Created Mar 24, 2015.
+ * @author songm. Created Mar 24, 2015.
  */
 public class Test {
 
 	@org.junit.Test
 	public void test() {
 		PrimeNumberGenerator g = new PrimeNumberGenerator();
-		
+
+	}
+
+	@org.junit.Test
+	public void testForEmpty() {
+
+		assertEquals(list(), PrimeNumberGenerator.generate(1));
+	}
+
+	private List<Integer> list() {
+		return Arrays.asList();
 	}
 
 }
