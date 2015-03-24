60a5c360f1da119d2df62a3cfce9102d2bfbcc78
songm <songm@rose-hulman.edu>
2015-03-24 14:07:45 -0400

    Fixed test 4

--- src/PrimeNumberGenerator.java
+++ src/PrimeNumberGenerator.java
@@ -10,10 +10,12 @@ public class PrimeNumberGenerator {
 
 	public static List<Integer> generate(int i) {
 		ArrayList<Integer> primes = new ArrayList<Integer>();
-		if(i>1){
+		if (i % 2 == 0) {
 			primes.add(2);
-		}else if(i>2){
-			primes.add(3);
+			i /= 2;
+		}
+		if (i > 1) {
+			primes.add(i);
 		}
 		return primes;
 	}
