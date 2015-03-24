f0d90eb353fdacc93470cc387fcc08ce686e1fc0
songm <songm@rose-hulman.edu>
2015-03-24 14:10:01 -0400

    Fixed test 8

--- src/PrimeNumberGenerator.java
+++ src/PrimeNumberGenerator.java
@@ -10,9 +10,9 @@ public class PrimeNumberGenerator {
 
 	public static List<Integer> generate(int i) {
 		ArrayList<Integer> primes = new ArrayList<Integer>();
-		if (i % 2 == 0) {
+		for (; i % 2 == 0; i /= 2) {
 			primes.add(2);
-			i /= 2;
+
 		}
 		if (i > 1) {
 			primes.add(i);
